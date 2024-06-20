4c331102c3dfd2f47b29ce1eabc1e8ed6891d2ba
porti@Portillo17 <portillo_andres17@yahoo.com>
2024-06-19 23:24:37 -0400

    work on trying to get the task made

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+// Todo: create a function to generate a unique task id
+function generateTaskId() {
+}
+// Todo: create a function to create a task card
+function createTaskCard(task) {
+}
+// Todo: create a function to render the task list and make cards draggable
+function renderTaskList() {
+}
+// Todo: create a function to handle adding a new task
+function handleAddTask(event){
+}
+// Todo: create a function to handle deleting a task
+function handleDeleteTask(event){
+}
+// Todo: create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+}
+// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {
+    $( "#datePicker" ).datepicker();
+});
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+var button = document.getElementById ("button");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// when the user press it it has to created the block
+// button.onclick = function(task description) {
+//   if (event.target == task description) {
+//     modal.style.display = "block";
+//   }
+// }
